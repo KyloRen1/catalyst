4d033fb4dc79bde481c42f32cb78ce9329682a55
Bogdan Ivanyuk <35574157+KyloRen1@users.noreply.github.com>
2019-11-18 13:16:56 +0200

    implemented symmetric ce

--- catalyst/contrib/criterion/ce.py
+++ catalyst/contrib/criterion/ce.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class NaiveCrossEntropyLoss(nn.Module):
     def __init__(self, size_average=True):
         super().__init__()
@@ -14,3 +13,23 @@ class NaiveCrossEntropyLoss(nn.Module):
         loss = -torch.sum(input * target)
         loss = loss / input.size()[0] if self.size_average else loss
         return loss
+
+class SymmetricCrossEntropyLoss(nn.Module):
+    def __init__(self, alpha, beta, num_classes):
+	super(SymmetricCrossEntropyLoss, self).__init__()
+        self.alpha = alpha
+	self.beta = beta
+	self.num_classes = num_classes
+		
+    def forward(self, input, target):
+	target_one_hot = F.one_hot(target.to(torch.int64), self.num_classes).float() 
+	assert target_one_hot.shape == input.shape
+
+	input = torch.clamp(input, min=1e-7, max=1.0)
+	target_one_hot = torch.clamp(target_one_hot, min=1e-4, max=1.0)
+
+	cross_entropy = (-torch.sum(target_one_hot * torch.log(input), dim=1)).mean()
+	reverse_cross_entropy = (-torch.sum(input * torch.log(target_one_hot), dim=1)).mean()
+	loss = self.alpha * cross_entropy + self.beta * reverse_cross_entropy
+	return loss
+        
